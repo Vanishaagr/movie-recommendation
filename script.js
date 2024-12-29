fdaed06a217c23ea7c648db2084b2adef8813a46
Vanishaagr <vanishaagarwal1407@gmail.com>
2024-12-29 20:01:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,246 @@
+let users = [
+    {
+       profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+       displayPic:"https://static.toiimg.com/thumb/msid-82139374,width-1280,height-720,imgsize-1544079,resizemode-6,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+       pendingMessage: 4,
+       location: "Pranav Mohanlal, India",
+       name: "Hridayam",
+       age: '',
+       interests: [{
+        icon: `<i class="ri-heart-fill"></i>`,
+        interest: "Romance"
+       },{
+        icon: `<i class="ri-movie-2-line"></i>`,
+        interest: "Drama"
+       }],
+       bio: "Malayalam film that explores the emotional journey of a young man named Arun, whose life is marked by love, heartbreak, and self-discovery, it captures the essence of growing up and finding oneself.",
+       isFriend: null
+    },
+    {
+       profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+       displayPic:"https://4kwallpapers.com/images/wallpapers/deadpool-wolverine-2560x1440-17093.jpg",
+       pendingMessage: 4,
+       location: "Ryan Reynolds, Hollywood",
+       name: "Deadpool & Wolverine",
+       age: '',
+       interests: [{
+        icon: `<i class="ri-flight-takeoff-line"></i>`,
+        interest: "Action"
+       },{
+        icon: `<i class="ri-emotion-laugh-line"></i>`,
+        interest: "Comedy"
+       }],
+       bio: "Deadpool & Wolverine unite in this high-octane adventure, combining Deadpool's irreverent humor and meta-commentary with Wolverine's raw, gritty intensity.",
+       isFriend: null
+    },
+     {
+         profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+         displayPic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmnhPh5mJmCyPpAi04-1lXUkalKWEYL35BDw&s",
+         pendingMessage: 4,
+         location: "Tom Cruise, Hollywood",
+         name: "Top Gun Maverick",
+         age: '',
+         interests: [{
+          icon: `<i class="ri-flight-takeoff-line"></i>`,
+          interest: "Action"
+         },{
+          icon: `<i class="ri-movie-2-line"></i>`,
+          interest: "Drama"
+         }],
+         bio: "Sequel to the iconic 1986 film “Top Gun.” Story revolves around Maverick confronting his past while training a group of younger Top Gun graduates for a dangerous mission.",
+         isFriend: null
+      },
+      {
+       profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+       displayPic:"https://assets.telegraphindia.com/telegraph/2024/Jun/1717580397_maharaja.jpg",
+       pendingMessage: 4,
+       location: "Vijay Sethupati, India",
+       name: "Maharaja",
+       age: '',
+       interests: [{
+        icon: `<i class="ri-flight-takeoff-line"></i>`,
+        interest: "Action"
+       },{
+        icon: `<i class="ri-movie-2-line"></i>`,
+        interest: "Thriller"
+       }],
+       bio: "A barber seeks vengeance after his home is burglarized, cryptically telling police his lakshmi has been taken, leaving them uncertain if it's a person or object.",
+       isFriend: null
+    },
+      {  
+         profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+         displayPic: "https://hdqwalls.com/download/joker-the-dark-knight-4k-img-1440x2960.jpg",
+         pendingMessage: 3,
+         location: "Christian Bale, Hollywood",
+         name: "The Dark Knight",
+         age: '',
+         interests: [{
+          icon: `<i class="ri-flight-takeoff-line"></i>`,
+          interest: "action"
+         },{
+          icon: `<i class="ri-movie-2-line"></i>`,
+          interest: "thriller"
+         }],
+         bio: "Directed by Christopher Nolan, stars Christian Bale as Batman battling the Joker, played by Heath Ledger. It's acclaimed for its intense action and deep, complex characters.",
+         isFriend: null
+      },
+      {
+         profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+         displayPic: "https://image.tmdb.org/t/p/original/s5HBKH7hNo8EElJTFQPEDwYgXhG.jpg",
+         pendingMessage: 2,
+         location: "Malyalam Cinema",
+         name: "Manjummel Boys",
+         age: '',
+         interests: [{
+          icon: `<i class="ri-treasure-map-line"></i>`,
+          interest: "Adventure"
+         },{
+          icon: `<i class="ri-movie-2-line"></i>`,
+          interest: "drama"
+         }],
+         bio: "Manjummel Boys is a heartwarming film about the adventures and camaraderie of friends from the Manjummel neighborhood, capturing the essence of friendship and youthful dreams.",
+         isFriend: null
+      },
+      {
+         profilePic: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlfGVufDB8fDB8fHww",
+         displayPic: "https://gamersstandard.com/wp-content/uploads/2023/07/everything-everywhere.jpg?w=816",
+         pendingMessage: 5,
+         location: "Evelyn Quan Wang, Hollywood",
+         name: "Everything Everywhere All at Once",
+         age: '',
+         interests: [{
+          icon: `<i class="ri-emotion-laugh-line"></i>`,
+          interest: "Comedy"
+         },{
+          icon: `<i class="ri-magic-line"></i>`,
+          interest: "Fantasy"
+         }],
+         bio: "Oscar winning sci-fi comedy where a woman connects with parallel universe versions of herself to stop a multiversal threat.",
+         isFriend: null
+      },
+ ];
+ 
+ function select(elem){
+     return document.querySelector(elem);
+ }
+ 
+ let curr = 0;
+ let isAnimating = false;
+ 
+ function setData(index){
+     select(".prlfimg img").src = users[index].profilePic;
+ 
+     select(".badge h5").textContent = users[index].pendingMessage;
+ 
+     select(".location h3").textContent = users[index].location;
+ 
+     select(".name h1:nth-child(1)").textContent = users[index].name;
+ 
+     select(".name h1:nth-child(2)").textContent = users[index].age;
+ 
+     let clutter = "";
+     users[index].interests.forEach(function(interest){
+       clutter +=`<div class="tag flex items-center bg-white/30 px-3 py-1 rounded-full gap-3">
+       ${interest.icon}
+       <h3 class="text-sm tracking-tight capitalize">${interest.interest}</h3>
+   </div>`
+     })
+     select(".tags").innerHTML = clutter;
+ 
+     select(".bio p").textContent = users[index].bio;
+ 
+ }
+ 
+ (function setInitial(){
+     select(".maincard img").src = users[curr].displayPic;
+ 
+     select(".incomingcard img").src = users[curr+1]?.displayPic;
+ 
+     setData(curr);
+ 
+     curr = 2;
+ 
+ })();
+ 
+ function imageChange(){
+    if(!isAnimating){
+       isAnimating = true;
+       let tl = gsap.timeline({
+ 
+          onComplete: function(){
+             isAnimating = false;
+             let main = select(".maincard");
+             let incoming = select(".incomingcard");
+    
+             incoming.classList.remove("z-[2]");
+             incoming.classList.add("z-[3]");
+             incoming.classList.remove("incomingcard");
+    
+             main.classList.remove("z-[3]");
+             main.classList.add("z-[2]");
+             gsap.set(main,{
+                scale: 1,
+                opacity: 1
+             })
+             if(curr === users.length) curr = 0;
+             select(".maincard img").src = users[curr].displayPic;
+             curr++;
+             main.classList.remove("maincard");
+             incoming.classList.add("maincard");
+             main.classList.add("incomingcard");
+          }
+       });
+    
+       tl.to(".maincard",{
+          scale: 1.1,
+          opacity: 0,
+          ease: Circ,
+          duration: .9
+       },"a")
+    
+       tl.to(".incomingcard",{
+          scale: .9,
+          opacity: 1,
+          ease: Circ,
+          duration: 1.1
+       },"a")
+    }
+ }
+ 
+ let deny = select(".deny");
+ let accept = select(".accept");
+ 
+ deny.addEventListener("click", function(){
+     imageChange();
+     setData(curr-1);
+       gsap.from(".details .element",{
+          y: "100%",
+          stagger: .06,
+          ease: Power4.easeInOut,
+          duration: 1.5
+       })
+       
+ });
+ 
+ accept.addEventListener("click", function(){
+    imageChange();
+    setData(curr-1);
+      gsap.from(".details .element",{
+         y: "100%",
+         stagger: .06,
+         ease: Power4.easeInOut,
+         duration: 1.5
+      })
+      
+ });
+ 
+ (function containerCreator(){
+       document.querySelectorAll(".element")
+       .forEach(function (element){
+          let div = document.createElement("div");
+          div.classList.add(`${element.classList[1]}container`, 'overflow-hidden');
+          div.appendChild(element);
+          select(".details").appendChild(div);
+       })
+ })();
+ 
